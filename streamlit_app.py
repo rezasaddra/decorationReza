2541f2a930316e836484791ba08d2d344b317bec
rezasaddra <71500451+rezasaddra@users.noreply.github.com>
2024-02-14 13:19:04 +0330

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+# pip install -q -U google-generativeai
+# pip install ipython
+# pip install pillow
+# pip install streamlit
+
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+import google.generativeai as genai
+from deep_translator.google import GoogleTranslator
+
+#api = st.text_input('put your api')
+
+API = "AIzaSyBlbIX0cwLZKqgd6866VYd2p8LYFp3_6Fo"
+
+genai.configure(api_key=API)
+model = genai.GenerativeModel('gemini-pro-vision')
+picture = st.camera_input("Take a picture")
+pr = 'How can i improve Design of my room ? and tell me what you see in the image'
+but = st.button('Lets Go')
+if picture and but :
+    img = Image.open(picture)
+    response = model.generate_content([pr , img])
+    st.image(img)
+    st.markdown(response.text)
+    translated = GoogleTranslator(source='auto', target='fa').translate(response.text)
+    st.markdown(translated)
+    
+st.info('Developed By : Reza Bahmani')
